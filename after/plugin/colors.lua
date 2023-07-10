b810ac82d29cd4db2f046f081ed245dffae205ce
Nikita Romaniuk <kelvium@yahoo.com>
2023-07-10 19:11:56 +0300

    colours: candle-grey + custom comment colour

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,3 +1,6 @@
 vim.opt.background = 'dark'
 vim.opt.termguicolors = true
-vim.cmd('colorscheme ayu-dark')
+vim.cmd('colorscheme candle-grey-transparent')
+
+-- test
+vim.cmd("hi Comment ctermfg=Gray ctermbg=NONE cterm=NONE guifg=#9a9a9a guibg=NONE gui=NONE")
